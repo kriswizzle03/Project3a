1e676fc638075b8afd3bdff6d4651646546fc194
kriswizzle03 <krislmor21@gmail.com>
2024-11-08 14:37:33 -0600

    Updated errors so selections can't be left blank. Added error-checking to make sure ending date is after starting date

--- app.py
+++ app.py
@@ -32,50 +32,50 @@ def index():
     if request.method == 'POST':
 
         #get the form data
-        #display errors if no selections are made
         stock_symbol = request.form.get('stock')
+        chart_type = request.form.get('graph')
+        time_series_function = request.form.get('time_series')
+        begin_date = request.form.get('start_date')
+        end_date = request.form.get('end_date')
+        
+        # display errors if no selections are made
         if not stock_symbol:
             flash("You must select a stock symbol!")
-
-        chart_type = request.form.get('graph')
-        if not chart_type:
+        elif not chart_type:
             flash("You must select a chart type!")
-
-        time_series_function = request.form.get('time_series')
-        if not time_series_function:
+        elif not time_series_function:
             flash("You must select a time series!")
-        
-        begin_date = request.form.get('start_date')
-        if not begin_date:
+        elif not begin_date:
             flash("You must select a start date!")
-
-        end_date = request.form.get('end_date')
-        if not end_date:
+        elif not end_date:
             flash("You must select an end date!")
-
-        #add error-checking for begin and end dates
-        print(stock_symbol, chart_type, time_series_function, begin_date, end_date)
+        # display error if end date is before begin date
+        elif end_date < begin_date:
+            flash("End date cannot be before Start Date")
+        
+        # print(stock_symbol, chart_type, time_series_function, begin_date, end_date)
     
+        else:
         #generate url for api
-        time_series_name = convert_time_series(time_series_function)
-        url = construct_url(BASE_URL, time_series_name, stock_symbol, INTERVAL, API_KEY)
+            time_series_name = convert_time_series(time_series_function)
+            url = construct_url(BASE_URL, time_series_name, stock_symbol, INTERVAL, API_KEY)
 
-        try:
-            # Get JSON data from API
-            response = requests.get(url)
-            # Raise an HTTPError if the HTTP request gives unsuccessful status code
-            response.raise_for_status() 
-            stock_data = response.json()
-            
-            # Check to see if full json stock data loaded
-            # If full json stock data is loaded, generate graph with user's selections
-            if not stock_data:
-                print("Error fetching stock data: please check the stock symbol and try again.")
-            else:
-                chart = generate_graph(stock_symbol, stock_data, chart_type, begin_date, end_date)
+            try:
+                # Get JSON data from API
+                response = requests.get(url)
+                # Raise an HTTPError if the HTTP request gives unsuccessful status code
+                response.raise_for_status() 
+                stock_data = response.json()
                 
-        except:
-            print("Failed to gather necessary inputs. Please restart the program.")
+                # Check to see if full json stock data loaded
+                # If full json stock data is loaded, generate graph with user's selections
+                if not stock_data:
+                    print("Error fetching stock data: please check the stock symbol and try again.")
+                else:
+                    chart = generate_graph(stock_symbol, stock_data, chart_type, begin_date, end_date)
+                    
+            except:
+                print("Failed to gather necessary inputs. Please restart the program.")
 
 
     return render_template('index.html', stock_list=stocks, chart=chart)
